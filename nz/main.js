5181d5f76766daf8fe99fdc8366f5f30f6eb95b7
Alexandra Buck <alkimbu@Alexandras-MacBook-Pro.local>
2020-03-27 11:25:32 +0100

    Hinzufügen der Karte

--- nz/main.js
+++ nz/main.js
@@ -1,1 +1,11 @@
 //alert("Hallo Welt!")
+
+var mymap = L.map('map').setView([data - lat = "-41.293056"
+    data - lng = "174.001944"
+], 13);
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(mymap);
+
