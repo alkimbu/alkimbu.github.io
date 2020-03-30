8dd554725b3b880236d9ff120e6373a9bd2be996
Alexandra Buck <alkimbu@Alexandras-MacBook-Pro.local>
2020-03-30 12:40:41 +0200

    EInbinden der Karte

--- nz/main.js
+++ nz/main.js
@@ -1,9 +1,15 @@
-//alert("Hallo Welt!")
+// Infos aus html abrufen
 
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+let map = document.querySelector("#map");
+let lat = map.dataset.lat;
+let lng = map.dataset.lng;
+let markerTitle = map.dataset.title;
+
+// Darstellung der Karte
+
+var mymap = L.map('map').setView([lat, lng], 13);
 
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap);
-
