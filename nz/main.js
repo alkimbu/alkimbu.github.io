a2062009313efbfb68187148293902bb28db40e7
Alexandra Buck <alkimbu@Alexandras-MacBook-Pro.local>
2020-03-30 15:16:30 +0200

    Marker einfügen

--- nz/main.js
+++ nz/main.js
@@ -13,3 +13,9 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap);
+
+// Marker einfügen
+
+let marker = L.marker([lat, lng]).addTo(mymap);
+
+marker.bindPopup(markerTitle).openPopup();
