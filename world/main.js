79a8b5774c56619d0dc1029f3cae8018b2a2f411
Alexandra Buck <alkimbu@Alexandras-MacBook-Pro.local>
2020-04-02 17:04:10 +0200

    Layer hinzufügen

---
+++ world/main.js
@@ -0,0 +1,18 @@
+let startLayer = L.tileLayer.provider("OpenTopoMap");
+let map = L.map("map", {
+    center: [0,0],
+    zoom: 2,
+    layers: [
+        startLayer
+    ]
+});
+
+L.control.layers({
+    "OpenTopoMap" : startLayer,
+    "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Stamen.Watercolor" :  L.tileLayer.provider("Stamen.Watercolor"),
+    "Stamen.TerrainBackground" :  L.tileLayer.provider("Stamen.TerrainBackground"),
+    "Esri.WorldImagery" :  L.tileLayer.provider("Esri.WorldImagery"),
+
+
+}).addTo(map)
