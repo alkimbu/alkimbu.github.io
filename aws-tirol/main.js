d9bc9af34d4d527d74a64db2635dc75d7ed2b799
Alexandra Buck <alkimbu@Alexandras-MacBook-Pro.local>
2020-04-23 17:11:00 +0200

    Windlayer implementiert

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -7,8 +7,9 @@ let map = L.map("map", {
 });
 
 let overlay = {
-    stations: L.featureGroup(), 
+    stations: L.featureGroup(),
     temperature: L.featureGroup(),
+    wind: L.featureGroup()
 }
 
 L.control.layers({
@@ -25,72 +26,80 @@ L.control.layers({
     ])
 }, {
     "Wetterstationen Tirol": overlay.stations,
-    "Temperatur °C": overlay.temperature
+    "Temperatur (°C)": overlay.temperature,
+    "Windgeschwindigkeit (km/h)": overlay.wind
 }).addTo(map);
 
 let awsUrl = "https://aws.openweb.cc/stations";
 
-
 let aws = L.geoJson.ajax(awsUrl, {
     filter: function (feature) {
-        // return feature.geometry.coordinates[2] > 3000;
-        // if (feature.properties.LT < 5) {
-        //     return true;
-        // } else {
-        // return false; 
-        // } 
-        // return feature.properties.LT < 5; 
+        //console.log("Feature in filter: ", feature);
         return feature.properties.LT;
     },
-
-
-
-
-pointToLayer: function (point, latlng) {
-    let marker = L.marker(latlng);
-    popupText = `<h3>${point.properties.name} ${point.geometry.coordinates[2]} </h3>
-                <ul>
-                <li><b>Position:</b> Lat: ${point.geometry.coordinates[0]}/Lng: ${point.geometry.coordinates[1]}</li>
-                <li><b>Datum:</b> ${point.properties.date}</li>
-                <li><b>Temperatur:</b> ${point.properties.LT} °C</li>
-                <li><b>Windgeschwindigkeit:</b> ${point.properties.WG} m/s</li>
-                <li><b>Relative Luftfeuchte:</b> ${point.properties.RH} %</li>
-                <li><b>Schneehöhe:</b> ${point.properties.HS} cm</li>
-                </ul>
-                <a target="links" href="https://lawine.tirol.gv.at/data/grafiken/1100/standard/tag/${point.properties.plot}.png">>> Graphik der Wetterstation</a>`
-                ;
-              
-
-    marker.bindPopup(popupText); 
-    return marker;
+    pointToLayer: function (point, latlng) {
+        // console.log("point: ", point);
+        let marker = L.marker(latlng).bindPopup(`
+        <h3>${point.properties.name} ${point.geometry.coordinates[2]} m</h3>
+        <ul>
+        <li>Datum: ${point.properties.date}</li>
+        <li>Position (Lat,Lng): ${point.geometry.coordinates[1].toFixed(5)}, ${point.geometry.coordinates[0].toFixed(5)}</li>
+        <li>Lufttemperatur (°C): ${point.properties.LT}</li>
+        <li>Windgeschwindigkeit (m/s): ${point.properties.WG || "-"}</li>
+        <li>Relative Luftfeuchte (%): ${point.properties.RH || "-"}</li>
+        <li>Schneehöhe (cm): ${point.properties.HS || "-"}</li>
+        </ul>
+        <p><a target="plot" href="https://lawine.tirol.gv.at/data/grafiken/1100/standard/tag/${point.properties.plot}.png">Grafik der vorhandenen Messwerte anzeigen</a></p>
+        </ul>
+        `);
+        return marker;
     }
 }).addTo(overlay.stations);
 
 let drawTemperature = function(jsonData) {
-    console.log("aus der Funktion", jsonData);
+    //console.log("aus der Funktion", jsonData);
     L.geoJson(jsonData, {
         filter: function(feature) {
-            return feature.properties.LT
+            return feature.properties.LT;
         },
         pointToLayer: function(feature, latlng) {
             return L.marker(latlng, {
                 title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m)`,
                 icon: L.divIcon({
                     html: `<div class="label-temperature">${feature.properties.LT.toFixed(1)}</div>`,
-                    className: "ignore-me" //dirty hack
+                    className: "ignore-me" // dirty hack
                 })
             })
         }
     }).addTo(overlay.temperature);
-   
+};
+
+
+
+let drawWind = function(jsonData) {
+    //console.log("aus der Funktion", jsonData);
+    L.geoJson(jsonData, {
+        filter: function(feature) {
+            return feature.properties.WG;
+        },
+        pointToLayer: function(feature, latlng) {
+            let kmh = Math.round(feature.properties.WG / 1000 * 3600);
+            return L.marker(latlng, {
+                title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m)`,
+                icon: L.divIcon({
+                    html: `<div class="label-wind">${kmh}</div>`,
+                    className: "ignore-me" // dirty hack
+                })
+            })
+        }
+    }).addTo(overlay.wind);
 };
 
 aws.on("data:loaded", function() {
-    // console.log(aws.toGeoJSON());
+    //console.log(aws.toGeoJSON());
     drawTemperature(aws.toGeoJSON());
-
+    drawWind(aws.toGeoJSON());
     map.fitBounds(overlay.stations.getBounds());
 
-    overlay.temperature.addTo(map);
-
+    overlay.wind.addTo(map);
 });
