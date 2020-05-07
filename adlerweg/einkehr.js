5d6439454352da50fac9a89157d43dcc5be90e4d
Alexandra Buck <alkimbu@Alexandras-MacBook-Pro.local>
2020-05-07 09:40:56 +0200

    Einkehrmöglichkeiten hinzugefügt

--- adlerweg/einkehr.js
+++ adlerweg/einkehr.js
@@ -22,5 +22,42 @@ const EINKEHR = [{
         "name": "Wuerttemberger Haus",
         "lat": 47.21,
         "lng": 10.535278
-    }
+    },
+    {
+        "etappe": 21,
+        "name": "Memminger Hütte",
+        "lat": 47.200637,
+        "lng": 10.478605
+    },
+    {
+        "etappe": 22,
+        "name": "Ansbacher Hütte",
+        "lat": 47.168505,
+        "lng": 10.403163
+
+    },
+    {
+        "etappe": 23,
+        "name": "Kaiserjochhaus",
+        "lat": 47.241030,
+        "lng": 10.334136
+
+    },
+    {
+        "etappe": 24,
+        "name": "Leutkircher Hütte",
+        "lat": 47.163514,
+        "lng": 10.285501
+
+    },
+
+    {
+        "etappe": 24,
+        "name": "Ulmer Hütte",
+        "lat": 47.147311,
+        "lng": 10.209339
+
+    },
+
+
 ]
