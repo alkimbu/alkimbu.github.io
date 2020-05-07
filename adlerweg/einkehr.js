59fd82267ab62226514ee049728ddb5bc31d36e2
Alexandra Buck <alkimbu@Alexandras-MacBook-Pro.local>
2020-05-07 09:27:05 +0200

    Einkehr.js hinzufügen

---
+++ adlerweg/einkehr.js
@@ -0,0 +1,26 @@
+const EINKEHR = [{
+        "etappe": 19,
+        "name": "Anhalter Huette",
+        "lat": 47.301111,
+        "lng": 10.667778
+
+    },
+    {
+        "etappe": 19,
+        "name": "Hanauer Huette",
+        "lat": 47.249722,
+        "lng": 10.590833
+    },
+    {
+        "etappe": 20,
+        "name": "Steinsee Huette",
+        "lat": 47.226111,
+        "lng": 10.587222
+    },
+    {
+        "etappe": 20,
+        "name": "Wuerttemberger Haus",
+        "lat": 47.21,
+        "lng": 10.535278
+    }
+]
